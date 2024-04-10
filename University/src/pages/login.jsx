608ed58ae13cd8eda682ae6a2aeb24b852618231
HosseinKolivand128 <ac.hossein.ac@gmail.com>
2024-04-10 16:53:33 +0330

    made signup login page

--- University/src/pages/login.jsx
+++ University/src/pages/login.jsx
@@ -1,51 +1,68 @@
 import React, { useState } from 'react';
 import {Link} from "react-router-dom"
-import Navbar from '../components/navbar';
 import background from "../assets/images/Signup.jpg"
 import style from "./signup.module.css"
-export default function Login(){
+export default function Signup(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const handleLogin = () => {
+    const [cpassword, setCPassword] = useState('');
+    const handleLogin = (e) => {
         // Implement login logic here
-        console.log('Logging in with:', username, password);
+        e.preventDefault();
+        if (password) {
+            alert(`Logedin with: ${username} ${password}`);
+            setUsername("");
+            setPassword("");
+        }else{
+            // document.querySelector("#username").style.borderColor = "red";
+            // document.querySelector("#password").style.borderColor = "red";
+            alert("Username or password does not matched");
+        }
     };
+
 return(<>
-    <Navbar/>
-        <section className={``} style={{background: `url(${background})`, height:"750px"}}>
-            <section className='p-5'>
-                <div className={`${style["form-container"]} container-lg rounded-5 d-flex flex-column gap-2 justify-content-around align-items-center mt-5`}>
-                    <div className='d-flex flex-column justify-content-around align-items-center gap-3'>
-                        <h2 className=''>ورود به حساب کاربری</h2>
-                        <Link to="/signup">حساب کاربری ندارید؟</Link>
-                    </div>
-                    <div className='d-flex flex-column align-items-center gap-3'>
-                       <div className='d-flex flex-column gap-2'>
-                            <label for="username" className='fs-5'>نام کاربری</label>
-                            <input
-                              className={`${style["input"]}`}
-                              id='username'
-                              type="text"
-                              placeholder="info@email"
-                              value={username}
-                              onChange={(e) => setUsername(e.target.value)}
-                            />
-                       </div>
-                       <div className='d-flex flex-column gap-2'>
-                            <label for="password" className='fs-5'>رمز عبور</label>
-                            <input
-                              className={`${style["input"]}`}
-                              id='password'
-                              type="password"
-                              placeholder="*****"
-                              value={password}
-                              onChange={(e) => setPassword(e.target.value)}
-                            />
-                       </div>
-                     <Link to="/signup">نیاز به کمک دارید؟</Link>
+            <section className={``} style={{background: `url(${background})`, backgroundSize:"cover",height:"100vh",width:"100vw",backgroundPosition:"center"}}>
+            <section className='p-5 '>
+                <div className={`${style["form-container"]} container-lg rounded-5 `}>
+
+                    <form action='#' method=''className={`d-flex flex-column gap-4 justify-content-around align-items-center mt-5`}>
+
+                    <div className='d-flex flex-column justify-content-around align-items-center'>
+                        <h2 className='fw-bold fs-1'style={{color:"#102c30",borderBottom:"1px solid black"}}>Login</h2>
 
                     </div>
-                    <button onClick={handleLogin} className={`${style["login"]} btn btn-success fs-4`}>ورود</button>
+                    <div className='d-flex flex-column align-items-center gap-5'>
+                       <div className='d-flex flex-column gap-2'>
+                            {/* <label for="username" className='fs-5 fw-bold '>Username</label> */}
+                            <input
+                            style={{color:"white"}}
+                              className={`${style["input"]} fw-bold fs-5`}
+                              id='username'
+                              type="text"
+                              placeholder="Enter your username"
+                              value={username}
+                              onChange={(e) => setUsername(e.target.value)}
+                              />
+                       </div>
+                       <div className='d-flex flex-column gap-3'>
+                            {/* <label for="password" className='fs-5 fw-bold '>Password</label> */}
+                            <input
+                            style={{color:"white"}}
+                              className={`${style["input"]} fw-bold fs-5`}
+                              id='password'
+                              type="password"
+                              placeholder="Enter your password"
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                              />
+                       </div>
+                       <div className='d-flex align-items-center flex-column gap-3'>
+                        <Link to="/login" style={{color:"gold"}}>Need help?</Link>
+                        <Link to="/signup" style={{color:"gold"}}>Don't have an account?</Link>
+                       </div>
+                    </div>
+                    <button onClick={handleLogin} style={{backgroundColor:"#0e2c31",color:"gold"}} className={`${style["login"]} btn fs-5 fw-bold mt-1`}>Login</button>
+                    </form>
                 </div>
             </section>
         </section>
--- University/src/pages/signup.jsx
+++ University/src/pages/signup.jsx
@@ -1,61 +1,76 @@
 import React, { useState } from 'react';
 import {Link} from "react-router-dom"
-import Navbar from '../components/navbar';
 import background from "../assets/images/Signup.jpg"
 import style from "./signup.module.css"
 export default function Signup(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [cpassword, setCPassword] = useState('');
-    const handleLogin = () => {
+    const handleLogin = (e) => {
         // Implement login logic here
-        console.log('Logging in with:', username, password);
+        e.preventDefault();
+        if (cpassword===password) {
+            alert(`Singed in with: ${username} ${password}`);
+            setUsername("");
+            setPassword("");
+            setCPassword("");
+        }else{
+            alert("Passwords field's are not same");
+
+        }
     };
 return(<>
-    <Navbar/>
-        <section className={``} style={{background: `url(${background})`, height:"750px"}}>
-            <section className='p-5'>
-                <div className={`${style["form-container"]} container-lg rounded-5 d-flex flex-column gap-2 justify-content-around align-items-center mt-5`}>
+            <section className={``} style={{background: `url(${background})`, backgroundSize:"cover",height:"100vh",width:"100vw",backgroundPosition:"center"}}>
+            <section className='p-5 '>
+                <div className={`${style["form-container"]} container-lg rounded-5 `}>
+
+                    <form action='#' method=''className={`d-flex flex-column gap-3 justify-content-around align-items-center mt-5`}>
+
                     <div className='d-flex flex-column justify-content-around align-items-center gap-3'>
-                        <h2 className=''>حساب کاربری بسازید</h2>
-                        <Link to="/login">حساب کاربری دارید؟</Link>
+                        <h2 className='fw-bold fs-1'style={{color:"#102c30",borderBottom:"1px solid black"}}>Sign up</h2>
+
+                        <Link to="/login" style={{color:"gold"}}>Have an account?</Link>
                     </div>
-                    <div className='d-flex flex-column align-items-center gap-3'>
+                    <div className='d-flex flex-column align-items-center gap-5'>
                        <div className='d-flex flex-column gap-2'>
-                            <label for="username" className='fs-5'>نام کاربری</label>
+                            {/* <label for="username" className='fs-5 fw-bold '>Username</label> */}
                             <input
-                              className={`${style["input"]}`}
+                            style={{color:"white"}}
+                              className={`${style["input"]} fw-bold fs-5`}
                               id='username'
                               type="text"
-                              placeholder="info@email"
+                              placeholder="Enter your username"
                               value={username}
                               onChange={(e) => setUsername(e.target.value)}
-                            />
+                              />
                        </div>
-                       <div className='d-flex flex-column gap-2'>
-                            <label for="password" className='fs-5'>رمز عبور</label>
+                       <div className='d-flex flex-column gap-3'>
+                            {/* <label for="password" className='fs-5 fw-bold '>Password</label> */}
                             <input
-                              className={`${style["input"]}`}
+                            style={{color:"white"}}
+                              className={`${style["input"]} fw-bold fs-5`}
                               id='password'
                               type="password"
-                              placeholder="*****"
+                              placeholder="Enter your password"
                               value={password}
                               onChange={(e) => setPassword(e.target.value)}
-                            />
+                              />
                        </div>
-                       <div className='d-flex flex-column gap-2'>
-                            <label for="cpassword" className='fs-5'>تایید رمز عبور</label>
+                       <div className='d-flex flex-column gap-3'>
+                            {/* <label for="cpassword" className='fs-5 fw-bold '>Confirm Password</label> */}
                             <input
-                              className={`${style["input"]}`}
+                            style={{color:"white"}}
+                              className={`${style["input"]} fw-bold fw-bold fs-5`}
                               id='cpassword'
                               type="password"
-                              placeholder="*****"
+                              placeholder="Confirm your password"
                               value={cpassword}
                               onChange={(e) => setCPassword(e.target.value)}
-                            />
+                              />
                        </div>
                     </div>
-                    <button onClick={handleLogin} className={`${style["login"]} btn btn-success fs-4`}>ثبت نام</button>
+                    <button onClick={handleLogin} style={{backgroundColor:"#0e2c31",color:"gold"}} className={`${style["login"]} btn fs-5 fw-bold mt-4`}>Sign up</button>
+                    </form>
                 </div>
             </section>
         </section>
